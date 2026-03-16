a2a43eb52cb1c05e617dfc6f07f270a22e7dc154
Laurent <ly.testud@outlook.com>
2026-03-16 14:09:49 -0700

    Separated VCard and MeCard

---
+++ Lyt.QrCode/Content/ContactCard.cs
@@ -0,0 +1,108 @@
+﻿namespace Lyt.QrCode.Content;
+
+#region Documentation 
+
+
+// VCARD : https://en.wikipedia.org/wiki/VCard 
+
+// MeCard : https://en.wikipedia.org/wiki/MeCard_(QR_code) 
+
+#endregion Documentation 
+
+public class ContactCard
+{
+    /// <summary> 
+    /// The address format.
+    /// European format: [Street] [House Number] and [Postal Code] [City] - Default
+    /// North American (and others): [House Number] [Street] and [City] [Postal Code]) 
+    /// </summary>
+    public enum AddressFormat
+    {
+        /// <summary> European format (Default) </summary>
+        European,
+
+        /// <summary> North American and others format. </summary>
+        NorthAmerica,
+    }
+
+    /// <summary> The kind of address (home or work). </summary>
+    /// <remarks>  VCard Only  </remarks>
+    public enum AddressKind
+    {
+        Home, // Default 
+        Work,
+        HomePref,
+        WorkPref,
+    }
+
+    public ContactCard(string firstName, string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+        {
+            throw new ArgumentException("First Name is required, cannot be null, empty or white space", nameof(firstName));
+        }
+
+        if (string.IsNullOrWhiteSpace(lastName))
+        {
+            throw new ArgumentException("Last Name is required, cannot be null, empty or white space", nameof(lastName));
+        }
+
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.BirthdayString = string.Empty;
+        this.Birthday = null;
+    }
+
+    public string FirstName { get; }
+
+    public string LastName { get; }
+
+    public AddressFormat Format { get; set; } = AddressFormat.European;
+
+    // All other relevant optional Card fields as properties defaulting to empty 
+    public string Nickname { get; set; } = string.Empty;
+
+    public string Organization { get; set; } = string.Empty;
+
+    public string OrganizationTitle { get; set; } = string.Empty;
+
+    public string Phone { get; set; } = string.Empty;
+
+    public string MobilePhone { get; set; } = string.Empty;
+
+    public string WorkPhone { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Website { get; set; } = string.Empty;
+
+    public string Street { get; set; } = string.Empty;
+
+    public string HouseNumber { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+
+    public string ZipCode { get; set; } = string.Empty;
+
+    public string StateRegion { get; set; } = string.Empty;
+
+    public string Country { get; set; } = string.Empty;
+
+    public string Note { get; set; } = string.Empty;
+
+    public string BirthdayString
+    {
+        get => field;
+        set
+        {
+            field = value;
+            if (DateTime.TryParse(value, out var date))
+            {
+                this.Birthday = date;
+            }
+        }
+    }
+
+    public DateTime? Birthday { get; set; } = null;
+
+}
--- Lyt.QrCode/Content/MeCardContent.cs
+++ Lyt.QrCode/Content/MeCardContent.cs
@@ -1,6 +1,102 @@
 ﻿namespace Lyt.QrCode.Content;
 
+using static Lyt.QrCode.Content.ContactCard;
 
-internal class MeCardContent
+public class MeCard(string firstName, string lastName) : ContactCard(firstName, lastName) { }
+
+internal sealed class MeCardContent(MeCard meCard) : QrContent<MeCard>(meCard)
 {
+    public override string RawString
+    {
+        get
+        {
+            var sb = new StringBuilder();
+            var card = this.Content;
+
+            // These may have been set to whitespace via properties after construction so we need to check
+            // again and set to empty if needed. 
+            string streetString = !string.IsNullOrWhiteSpace(card.Street) ? card.Street : "";
+            string houseNumberString = !string.IsNullOrWhiteSpace(card.HouseNumber) ? card.HouseNumber : "";
+            string cityString = !string.IsNullOrWhiteSpace(card.City) ? card.City : "";
+            string stateRegionString = !string.IsNullOrWhiteSpace(card.StateRegion) ? card.StateRegion : "";
+            string zipCodeString = !string.IsNullOrWhiteSpace(card.ZipCode) ? card.ZipCode : "";
+            string countryString = !string.IsNullOrWhiteSpace(card.Country) ? card.Country : "";
+
+            // No need to push an address if everything is empty 
+            bool allEmpty =
+                string.IsNullOrEmpty(streetString) &&
+                string.IsNullOrEmpty(houseNumberString) &&
+                string.IsNullOrEmpty(cityString) &&
+                string.IsNullOrEmpty(zipCodeString) &&
+                string.IsNullOrEmpty(countryString);
+
+            sb.Append("MECARD:");
+            sb.Append($"N:{card.LastName},{card.FirstName}");
+            if (!string.IsNullOrWhiteSpace(card.Nickname))
+            {
+                sb.Append($"NICKNAME:{card.Nickname};");
+            }
+
+            if (card.Birthday != null)
+            {
+                sb.Append($"BDAY:{((DateTime)card.Birthday).ToString("yyyyMMdd", CultureInfo.InvariantCulture)};");
+            }
+
+            if (!string.IsNullOrWhiteSpace(card.Organization))
+            {
+                sb.Append($"ORG:{card.Organization};");
+            }
+
+            if (!string.IsNullOrWhiteSpace(card.OrganizationTitle))
+            {
+                sb.Append($"TITLE:{card.OrganizationTitle};");
+            }
+
+            if (!string.IsNullOrWhiteSpace(card.Phone))
+            {
+                sb.Append($"TEL:{card.Phone};");
+            }
+
+            if (!string.IsNullOrWhiteSpace(card.MobilePhone))
+            {
+                sb.Append($"TEL:{card.MobilePhone};");
+            }
+
+            if (!string.IsNullOrWhiteSpace(card.WorkPhone))
+            {
+                sb.Append($"TEL:{card.WorkPhone};");
+            }
+
+            if (!string.IsNullOrWhiteSpace(card.Email))
+            {
+                sb.Append($"EMAIL:{card.Email};");
+            }
+
+            if (!string.IsNullOrWhiteSpace(card.Website))
+            {
+                sb.Append($"URL:{card.Website};");
+            }
+
+            if (!string.IsNullOrWhiteSpace(card.Note))
+            {
+                sb.Append($"NOTE:{card.Note};");
+            }
+
+            if (!allEmpty)
+            {
+                string streetHouse =
+                    card.Format == AddressFormat.European ?
+                        $"{streetString} {houseNumberString}" :
+                        $"{houseNumberString} {streetString}";
+                string addressString =
+                        $"ADR:,,{streetHouse},{cityString},{stateRegionString},{zipCodeString},{countryString};";
+                sb.Append(addressString);
+            }
+
+            // Terminator 
+            sb.Append(';');
+
+            return sb.ToString();
+        }
+    }
 }
