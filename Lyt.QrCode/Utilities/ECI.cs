b6d660220bd0a2dfb9ed5bcef88f04f57a91cf31
Laurent <ly.testud@outlook.com>
2026-03-19 13:46:08 -0700

    deleted ECI

--- Lyt.QrCode/Utilities/ECI.cs
+++
@@ -1,8 +0,0 @@
-namespace Lyt.QrCode.Utilities;
-
-/// <summary> Base class of classes representing ECIs, according to "Extended Channel Interpretations" 5.3 of ISO 18004. </summary>
-//internal abstract class ECI
-//{
-
-
-//}
