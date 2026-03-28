5c14ce9ce893ebe1a34c23d20bb3bd601ccf03a9
Laurent <ly.testud@outlook.com>
2026-03-28 16:22:25 -0700

    Incomplete demo code for GDI +

--- Lyt.QrCode.Decode.Demo/Demo.cs
+++ Lyt.QrCode.Decode.Demo/Demo.cs
@@ -21,11 +21,12 @@ internal sealed class Demo
 
     internal void Run()
     {
+        // TODO
     }
 
-    private SourceImage LoadSourceImage(string imagePath)
+    private SourceImage LoadSourceImageWithSixLabors(string imagePath)
     {
-        string fullPath = Path.Combine(rootPath, imagePath);
+        string fullPath = Path.Combine(this.rootPath, imagePath);
         using var image = SixLabors.ImageSharp.Image.Load<Rgba32>(fullPath);
         byte[] pixels = new byte[image.Width * image.Height * 4];
         image.CopyPixelDataTo(pixels);
@@ -36,4 +37,53 @@ internal sealed class Demo
         return sourceImage;
     }
 
+    /* 
+
+    WinForms / GDI+ 
+
+        using System.Drawing;
+    using System.IO;
+
+    public static Image LoadImageInMemory(string filePath)
+    {
+        // Use File.ReadAllBytes to load the file's data into a byte array
+        byte[] imageBytes = File.ReadAllBytes(filePath);
+
+        // Create a MemoryStream from the byte array
+        MemoryStream ms = new MemoryStream(imageBytes);
+
+        // Create the Image from the MemoryStream
+        Image image = Image.FromStream(ms);
+
+        // Note: The MemoryStream 'ms' must remain open for the lifetime of the 'image' object
+        // if using GDI+. To dispose of the stream immediately, you must make a copy of the 
+        // image, for example, into a new Bitmap.
+        // Bitmap bitmapCopy = new Bitmap(image); 
+        // ms.Dispose(); // You can then dispose of the stream
+
+        return image;
+    }
+
+
+    WPF / WIC 
+
+    using System.Windows.Media.Imaging;
+
+    public static BitmapImage LoadImageToMemory(string path)
+    {
+        BitmapImage image = new BitmapImage();
+        using (FileStream stream = File.Open(path, FileMode.Open))
+        {
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.StreamSource = new MemoryStream();
+            stream.CopyTo(image.StreamSource);
+            image.EndInit();
+        }
+        // The FileStream is closed here by the 'using' block, and because of 
+        // CacheOption.OnLoad, the image data is already in memory.
+        return image;
+    }
+
+    */
 }
