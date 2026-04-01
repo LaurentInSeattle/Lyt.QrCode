983c0b9a900e1cb2aae0bea1e9152d84bbb7cfff
Laurent <ly.testud@outlook.com>
2026-04-01 11:44:18 -0700

    Refactored Raw Png Data for use later in refactoring PNG builder

--- PngLibrary/Internals/PngImage.Load.cs
+++ PngLibrary/Internals/PngImage.Load.cs
@@ -2,14 +2,31 @@
 
 public partial class PngImage
 {
-    private readonly RawPngData data;
+    private readonly byte[] data;
+    private readonly int bytesPerPixel;
+    private readonly Palette? palette;
     private readonly bool hasTransparencyChunk;
 
-    internal PngImage(ImageHeader header, RawPngData data, bool hasTransparencyChunk)
+    // These fields are cached from the header for faster access when getting pixels.
+    private readonly int width;
+    private readonly ColorType colorType;
+    private readonly int rowOffset;
+    private readonly int bitDepth;
+
+    internal PngImage(
+        ImageHeader imageHeader, byte[] data, int bytesPerPixel, Palette? palette, bool hasTransparencyChunk)
     {
-        this.Header = header;
-        this.data = data ?? throw new ArgumentNullException(nameof(data));
+        this.Header = imageHeader;
+        this.data = data;
+        this.bytesPerPixel = bytesPerPixel;
+        this.palette = palette;
         this.hasTransparencyChunk = hasTransparencyChunk;
+
+        // These fields are cached from the header for faster access when getting pixels.
+        this.width = imageHeader.Width;
+        this.colorType = imageHeader.ColorType;
+        this.rowOffset = imageHeader.InterlaceMethod == InterlaceMethod.Adam7 ? 0 : 1;
+        this.bitDepth = imageHeader.BitDepth;
     }
 
     /// <summary> The header data from the PNG image. </summary>
@@ -199,8 +216,97 @@ public partial class PngImage
 
         return
             new PngImage(
-                imageHeader,
-                new RawPngData(bytesOut, bytesPerPixel, palette, imageHeader),
-                palette?.HasAlphaValues ?? false);
+                imageHeader, bytesOut, bytesPerPixel, palette, palette?.HasAlphaValues ?? false);
+    }
+
+    /// <summary> Get the pixel at the given column and row (x, y). </summary>
+    /// <remarks>
+    /// Pixel values are generated on demand from the underlying data to prevent holding many items in memory at once, 
+    /// so consumers should cache values if they're going to be looped over many time.
+    /// </remarks>
+    /// <param name="x">The x coordinate (column).</param>
+    /// <param name="y">The y coordinate (row).</param>
+    /// <returns>The pixel at the coordinate.</returns>
+    public Pixel GetPixel(int x, int y)
+    {
+        if (palette != null)
+        {
+            int pixelsPerByte = (8 / bitDepth);
+            int bytesInRow = (1 + (width / pixelsPerByte));
+            int byteIndexInRow = x / pixelsPerByte;
+            int paletteIndex = (1 + (y * bytesInRow)) + byteIndexInRow;
+            byte b = data[paletteIndex];
+
+            if (bitDepth == 8)
+            {
+                return palette.GetPixel(b);
+            }
+
+            int withinByteIndex = x % pixelsPerByte;
+            int rightShift = 8 - ((withinByteIndex + 1) * bitDepth);
+            int indexActual = (b >> rightShift) & ((1 << bitDepth) - 1);
+
+            return palette.GetPixel(indexActual);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static byte ToSingleByte(byte first, byte second)
+        {
+            int us = (first << 8) + second;
+            return (byte)Math.Round((255 * us) / (double)ushort.MaxValue);
+        }
+
+        int rowStartPixel = (rowOffset + (rowOffset * y)) + (bytesPerPixel * width * y);
+        int pixelStartIndex = rowStartPixel + (bytesPerPixel * x);
+        byte first = data[pixelStartIndex];
+
+        switch (bytesPerPixel)
+        {
+            case 1:
+                return new Pixel(first, first, first, 255, true);
+
+            case 2:
+                switch (colorType)
+                {
+                    case ColorType.None:
+                        {
+                            byte second = data[pixelStartIndex + 1];
+                            byte value = ToSingleByte(first, second);
+                            return new Pixel(value, value, value, 255, true);
+                        }
+
+                    default:
+                        return new Pixel(first, first, first, data[pixelStartIndex + 1], true);
+                }
+
+            case 3:
+                return new Pixel(first, data[pixelStartIndex + 1], data[pixelStartIndex + 2], 255, false);
+
+            case 4:
+                switch (colorType)
+                {
+                    case ColorType.None | ColorType.AlphaChannelUsed:
+                        {
+                            byte second = data[pixelStartIndex + 1];
+                            byte firstAlpha = data[pixelStartIndex + 2];
+                            byte secondAlpha = data[pixelStartIndex + 3];
+                            byte gray = ToSingleByte(first, second);
+                            byte alpha = ToSingleByte(firstAlpha, secondAlpha);
+                            return new Pixel(gray, gray, gray, alpha, true);
+                        }
+
+                    default:
+                        return new Pixel(first, data[pixelStartIndex + 1], data[pixelStartIndex + 2], data[pixelStartIndex + 3], false);
+                }
+            case 6:
+                return new Pixel(first, data[pixelStartIndex + 2], data[pixelStartIndex + 4], 255, false);
+
+            case 8:
+                return new Pixel(
+                    first, data[pixelStartIndex + 2], data[pixelStartIndex + 4], data[pixelStartIndex + 6], false);
+
+            default:
+                throw new InvalidOperationException($"Unreconized number of bytes per pixel: {bytesPerPixel}.");
+        }
     }
 }
--- PngLibrary/PngImage.cs
+++ PngLibrary/PngImage.cs
@@ -51,13 +51,5 @@ public partial class PngImage
     /// <summary> Whether the image has an alpha (transparency) layer. </summary>
     public bool HasAlphaChannel => (this.Header.ColorType & ColorType.AlphaChannelUsed) != 0 || this.hasTransparencyChunk;
 
-    /// <summary> Get the pixel at the given column and row (x, y). </summary>
-    /// <remarks>
-    /// Pixel values are generated on demand from the underlying data to prevent holding many items in memory at once, 
-    /// so consumers should cache values if they're going to be looped over many time.
-    /// </remarks>
-    /// <param name="x">The x coordinate (column).</param>
-    /// <param name="y">The y coordinate (row).</param>
-    /// <returns>The pixel at the coordinate.</returns>
-    public Pixel GetPixel(int x, int y) => data.GetPixel(x, y);
+    // public Pixel GetPixel(int x, int y) => data.GetPixel(x, y);
 }
